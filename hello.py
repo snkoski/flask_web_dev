8827ccc4d697604835082432c3acfbb43dcbcc95
snkoski <shawnnkoski@gmail.com>
2018-12-13 22:12:27 -0500

    Add templates directory with base.html, index.html, user.html, 404.html, 500.html templates. Add flask_bootstrap to project

--- hello.py
+++ hello.py
@@ -1,25 +1,31 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request, make_response, redirect, abort, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', temp_name=name)
 
 @app.route('/browser')
 def browser():
     user_agent = request.headers.get('User-Agent')
     return '<h1>Your browser is %s</h1>' % user_agent
-
-@app.route('/bad')
-def bad():
-    return '<h1>Bad Request</h1>', 400
 
 @app.route('/cookie')
 def cookie():
