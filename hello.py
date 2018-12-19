c03c7fb3d26fb71e68bb55615e000bed8d72f8e2
snkoski <shawnnkoski@gmail.com>
2018-12-19 16:14:17 -0500

    Added SQLAlchemy to project. MAde User and Role tables with sqlite. Made models for User and Role. Changed up index route to use sqlite db

--- hello.py
+++ hello.py
@@ -1,21 +1,53 @@
+import os
 from datetime import datetime
 from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/flask_book'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+# Readable string representation for debugging
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -29,13 +61,17 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -71,5 +107,5 @@ def get_filters():
     return render_template('filters.html', phrase=phrase, lower=lower, upper=upper)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # manager.run()
+#     app.run(debug=True)
+    manager.run()
